397585e8a20d442520c164d4cb4a983c96b7e171
PuzzleAd <mamulashviliirakli65@gmail.com>
2024-05-20 20:38:30 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("input");
+const plusButton = document.getElementById("plus-container");
+const taskText = document.getElementById("task-counter");
+const idBox = document.getElementById("id-box");
+
